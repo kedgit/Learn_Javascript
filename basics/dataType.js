ac12a86ed43aaa2f254120aa63433f6d861cb271
Kedar Mhetre <163038642+kedgit@users.noreply.github.com>
2025-12-27 07:10:14 +0000

    learn datatype in js with its type

---
+++ basics/dataType.js
@@ -0,0 +1,32 @@
+// code readability
+console.log("hi"); console.log("hello")
+
+let name="kedar"
+let age=23
+let isloggedIn=true
+/*
+    number --> 2
+    string 
+    bigint
+    boolean
+    null --> Standalone value (intensialy define null)
+    undefined --> only declare not assign value 
+    symbol--> unique ness
+    object --> 
+*/
+console.log(typeof "kedar");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 23);
+console.log(typeof true);
+
+/* @kedgit ➜ /workspaces/Learn_Javascript (main) $ node basics/dataType.js
+hi
+hello
+string
+object
+undefined
+number
+boolean
+
+ */
