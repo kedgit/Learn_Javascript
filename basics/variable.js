09d26afc23158d8e828b9ffae259d218e565c31e
Kedar Mhetre <163038642+kedgit@users.noreply.github.com>
2025-12-27 07:11:00 +0000

    learn vaiables

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const accountId=123 
+let email="kedar@mail.com"  // block scope 
+var pass="kedar@123"  // global scope
+city="pune"
+
+// not allow
+// accountId=24
+email="hari@gmail.com"  // reassign not redefine
+pass="735672"
+city="mumbai"
+let state
+console.table([email,pass,city,state])
