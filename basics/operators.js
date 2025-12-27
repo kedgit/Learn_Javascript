9778367aed6d0ac2358e6cd766e8912751e6aae6
Kedar Mhetre <163038642+kedgit@users.noreply.github.com>
2025-12-27 07:51:49 +0000

    for strict check use triple equal

---
+++ basics/operators.js
@@ -0,0 +1,13 @@
+// console.log(2>true)
+// console.log("2"==2);
+
+// console.log(null>0)
+// console.log(null==0);
+// console.log(null>=0);
+
+// strict check
+console.log(2=="2"); // true
+console.log(2==="2"); // false
+
+
+
