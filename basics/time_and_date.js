68cfab43f1231d1e74482e421edf57a2bbdd7c18
Kedar Mhetre <163038642+kedgit@users.noreply.github.com>
2025-12-29 10:15:49 +0000

    time and date concept

---
+++ basics/time_and_date.js
@@ -0,0 +1,13 @@
+let date=new Date();
+console.log(date)
+console.log(date.toString());
+console.log(date.toISOString());
+
+console.log(date.toJSON())
+console.log(date.toLocaleDateString());
+
+let today=new Date(2025,1,14);
+console.log(today.toLocaleDateString());
+
+let day= new Date("01/14/2025");
+console.log(day);
