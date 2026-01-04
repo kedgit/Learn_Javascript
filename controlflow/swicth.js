244a2bd83cb5087a41f0d1d85e4b25dcdae7848e
Kedar Mhetre <163038642+kedgit@users.noreply.github.com>
2026-01-04 07:32:45 +0000

    some truthy and falsy values with switch case

---
+++ controlflow/swicth.js
@@ -0,0 +1,27 @@
+// falsy value
+// 0,false,-0,null,undefined,Nan,"",BigInt 0n
+
+// truthy values
+// "0","false",{},[],function(){}," ",
+const month=3
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    default:
+        console.log("No");
+
+}
+
+// const s=""
+let a;
+if(a){
+    console.log("true");
+}
+else{
+    console.log("false");
+    
+}
